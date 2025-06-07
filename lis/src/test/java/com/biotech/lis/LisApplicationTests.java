3f5b3174adf2610d29b360f6dfc86934b3ae59db
Patrick Perez <patrick_hans_perez@dlsu.edu.ph>
2025-06-08 03:44:39 +0800

    feat: connected the backend to the cloud database

--- lis/src/test/java/com/biotech/lis/LisApplicationTests.java
+++ lis/src/test/java/com/biotech/lis/LisApplicationTests.java
@@ -2,12 +2,22 @@ package com.biotech.lis;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import javax.sql.DataSource;
+import java.sql.SQLException;
 
 @SpringBootTest
 class LisApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	//Database Connection Test
+	@Autowired
+    private DataSource dataSource;
 
+    @Test
+    public void testConnection() throws SQLException {
+        try (var connection = dataSource.getConnection()) {
+            System.out.println("Database: " + connection.getMetaData().getDatabaseProductName());
+            System.out.println("Connection successful!");
+        }
+    }
 }
