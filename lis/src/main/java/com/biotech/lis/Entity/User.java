e4060eef56028342aa6c5a06924e3edc1dff55aa
Patrick Perez <patrick_hans_perez@dlsu.edu.ph>
2025-06-09 15:12:15 +0800

    feat: added entities(models) in the backend, also added folder structure for CRUD ops

---
+++ lis/src/main/java/com/biotech/lis/Entity/User.java
@@ -0,0 +1,21 @@
+package com.biotech.lis.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue
+    private Integer user_id;
+    private String firstname;
+    private String lastname;
+    private char[] password;
+}
