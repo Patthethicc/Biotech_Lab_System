3050b232843536814f200e9250933c2cac63b7a6
AlexKaine <109214928+AlexKaine@users.noreply.github.com>
2025-06-12 14:29:18 +0800

    added sidebar which appears on large screeens and a bar on the bottom that appears on smaller screeens

---
+++ frontend/lib/pages/reporting.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Reporting extends StatelessWidget {
+  const Reporting({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(child: Text("reporting"),);
+  }
+}
