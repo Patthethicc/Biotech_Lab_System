8643007969e37d99484ca8c81d3a7d5b7ec826ce
Ethan Dela Cruz <165283236+ItsNasher@users.noreply.github.com>
2025-07-19 21:34:13 +0800

    added error handling

--- lis/src/main/java/com/biotech/lis/Service/PurchaseOrderService.java
+++ lis/src/main/java/com/biotech/lis/Service/PurchaseOrderService.java
@@ -5,10 +5,10 @@ import com.biotech.lis.Entity.PurchaseOrder;
 import com.biotech.lis.Repository.PurchaseOrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import java.util.Optional;
 
 @Service
 public class PurchaseOrderService {
-
     @Autowired
     PurchaseOrderRepository purchaseOrderRepository;
 
@@ -16,8 +16,8 @@ public class PurchaseOrderService {
         return purchaseOrderRepository.save(purchaseOrder);
     }
 
-    public PurchaseOrder getPurchaseOrderByCode(String code) {
-        return purchaseOrderRepository.findByPurchaseOrderCode(code);
+    public Optional<PurchaseOrder> getPurchaseOrderByCode(String code) {
+        return Optional.ofNullable(purchaseOrderRepository.findByPurchaseOrderCode(code));
     }
 
     public PurchaseOrder updatePurchaseOrder(PurchaseOrder purchaseOrder) {
@@ -26,6 +26,4 @@ public class PurchaseOrderService {
     public void deletePurchaseOrder(String code) {
         purchaseOrderRepository.deleteById(code);
     }
-
-
 }
