0698fb5689d78677ef6c078e2c25184ae8d0c728
KARPINTER <165571272+S16ADAVID@users.noreply.github.com>
2025-07-19 19:05:30 +0800

    Add files via upload

--- lis/src/main/java/com/biotech/lis/Repository/PurchaseOrderRepository.java
+++ lis/src/main/java/com/biotech/lis/Repository/PurchaseOrderRepository.java
@@ -1,11 +1,8 @@
 package com.biotech.lis.Repository;
 
-import java.util.List;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
 import com.biotech.lis.Entity.PurchaseOrder;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PurchaseOrderRepository extends JpaRepository<PurchaseOrder, String> {
+    public PurchaseOrder findByPurchaseOrderCode(String code);
 }
