46d48bace61707af07de758b3f66fabe00df1be8
Ethan Dela Cruz <165283236+ItsNasher@users.noreply.github.com>
2025-07-20 00:30:30 +0800

    Add files via upload

--- lis/src/main/java/com/biotech/lis/Service/PurchaseOrderService.java
+++ lis/src/main/java/com/biotech/lis/Service/PurchaseOrderService.java
@@ -2,9 +2,11 @@ package com.biotech.lis.Service;
 
 
 import com.biotech.lis.Entity.PurchaseOrder;
+import com.biotech.lis.Entity.User;
 import com.biotech.lis.Repository.PurchaseOrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -12,7 +14,18 @@ public class PurchaseOrderService {
     @Autowired
     PurchaseOrderRepository purchaseOrderRepository;
 
+//  @Autowired
+//  UserService userService;
+
     public PurchaseOrder addPurchaseOrder(PurchaseOrder purchaseOrder) {
+        /* Fix later because the addedby must be the userId because it is the primary key
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        User user = userService.getUserById(Long.parseLong(auth.getName()));
+        LocalDateTime cDateTime = LocalDateTime.now();
+        purchaseOrder.setAddedBy(user.getFirstName().concat(" " + user.getLastName()));
+        purchaseOrder.setDateTimeAdded(cDateTime);
+
+         */
         return purchaseOrderRepository.save(purchaseOrder);
     }
 
@@ -20,7 +33,18 @@ public class PurchaseOrderService {
         return Optional.ofNullable(purchaseOrderRepository.findByPurchaseOrderCode(code));
     }
 
+    public List<PurchaseOrder> getAllPurchaseOrders() {
+        return purchaseOrderRepository.findAll();
+    }
+
     public PurchaseOrder updatePurchaseOrder(PurchaseOrder purchaseOrder) {
+        /* Fix later because the addedby must be the userId because it is the primary key
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        User user = userService.getUserById(Long.parseLong(auth.getName()));
+        LocalDateTime cDateTime = LocalDateTime.now();
+        purchaseOrder.setAddedBy(user.getFirstName().concat(" " + user.getLastName()));
+        purchaseOrder.setDateTimeAdded(cDateTime);
+         */
         return purchaseOrderRepository.save(purchaseOrder);
     }
     public void deletePurchaseOrder(String code) {
