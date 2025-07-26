dc50ebb4e532d5987fc70760d95b88cbdfa3ea20
FineTyler <mendozatyrrelle@gmail.com>
2025-07-26 19:33:20 +0800

    lofi bar chart of highest and lowest stocks

--- frontend/lib/models/ui/home.dart
+++ frontend/lib/models/ui/home.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter_neumorphic_plus/flutter_neumorphic.dart';
 import 'package:frontend/services/dashboard_service.dart';
+import 'package:frontend/services/inventory_service.dart';
 import 'package:frontend/services/transaction_entry_service.dart';
+import 'package:frontend/models/api/inventory.dart';
 import 'login.dart';
 
 class HomePage extends StatefulWidget {
@@ -14,7 +17,11 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
-  
+
+  List<Inventory> top = [];
+  List<Inventory> bottom = [];
+  bool loadingTop = true, loadingBottom = true;
+
   int outOfStock = 0;
   String selectedPeriod = 'daily';
   int dynamicCount = 0;
@@ -22,10 +29,23 @@ class _HomePageState extends State<HomePage> {
   bool loading = true;
   bool isLoadingDynamicCount = true;
 
+  TextStyle headerStyle = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.bold,
+    color: Color(0xFF01579B), // Dark blue
+  );
+
+  TextStyle smallTitleStyle = TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.w500,
+    color: Color(0xFF01579B), // Consistent dark blue for labels
+  );
+
   @override
   void initState() {
     super.initState();
     _loadAllStats();
+    _loadInventories();
   }
 
   Future<void> _loadAllStats() async {
@@ -46,6 +66,27 @@ class _HomePageState extends State<HomePage> {
     }
   }
 
+  Future<void> _loadInventories() async {
+    setState(() {
+      loadingTop = true;
+      loadingBottom = true;
+    });
+    try {
+      final t = await InventoryService().getTopStock();
+      final b = await InventoryService().getBottomStock();
+      setState(() {
+        top = t;
+        bottom = b;
+      });
+    } catch (e) {
+      print('Stock load error: $e');
+    } finally {
+      setState(() {
+        loadingTop = false;
+        loadingBottom = false;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -54,7 +95,7 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         title: const Text('Biotech Home'),
         actions: [
-            DropdownButton<String>(
+          DropdownButton<String>(
             value: selectedPeriod,
             onChanged: (v) {
               if (v != null) {
@@ -62,10 +103,12 @@ class _HomePageState extends State<HomePage> {
                 _loadAllStats();
               }
             },
-            items: ['daily', 'monthly', 'yearly']
-              .map((p) => DropdownMenuItem(value: p, child: Text(p)))
-              .toList(),
-          )
+            items: [
+              'daily',
+              'monthly',
+              'yearly',
+            ].map((p) => DropdownMenuItem(value: p, child: Text(p))).toList(),
+          ),
         ],
       ),
       drawer: buildDrawer(context),
@@ -77,9 +120,10 @@ class _HomePageState extends State<HomePage> {
           Row(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              buildLeftColumn(),
+              buildChart('📊 Top Stock Items', top, loadingTop),
               const SizedBox(width: 20),
-              buildRightColumn(),
+              buildChart('🔻 Low Stock Items', bottom, loadingBottom),
+              // buildRightColumn(),
             ],
           ),
         ],
@@ -92,7 +136,11 @@ class _HomePageState extends State<HomePage> {
       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
       children: [
         summaryCard("Total Transactions", totalQuantity),
-        summaryCard(_cardLabel(), dynamicCount, isLoading: isLoadingDynamicCount),
+        summaryCard(
+          _cardLabel(),
+          dynamicCount,
+          isLoading: isLoadingDynamicCount,
+        ),
         summaryCard("Total Quantity", totalQuantity),
         summaryCard("Out of Stock", outOfStock),
       ],
@@ -101,132 +149,168 @@ class _HomePageState extends State<HomePage> {
 
   String _cardLabel() {
     switch (selectedPeriod) {
-      case 'daily': return "Today's Transactions";
-      case 'monthly': return "This Month's Transactions";
-      case 'yearly': return "This Year's Transactions";
-      default: return "Transactions";
+      case 'daily':
+        return "Today's Transactions";
+      case 'monthly':
+        return "This Month's Transactions";
+      case 'yearly':
+        return "This Year's Transactions";
+      default:
+        return "Transactions";
     }
   }
 
-Widget summaryCard(String title, int value, {bool isLoading = false}) {
-  return SizedBox(
-    width: 250,
-    height: 70,
-    child: Neumorphic(
-      style: NeumorphicStyle(
-        depth: -3,
-        color: const Color(0xFFE0E5EC),
-        intensity: 5.0,
-        shadowLightColorEmboss: const Color.fromARGB(255, 238, 243, 247),
-        shadowDarkColorEmboss: const Color.fromARGB(255, 213, 224, 235),
-        boxShape: NeumorphicBoxShape.roundRect(
-          BorderRadius.circular(100),
+  Widget summaryCard(String title, int value, {bool isLoading = false}) {
+    return SizedBox(
+      width: 250,
+      height: 70,
+      child: Neumorphic(
+        style: NeumorphicStyle(
+          depth: -3,
+          color: const Color(0xFFE0E5EC),
+          intensity: 5.0,
+          shadowLightColorEmboss: const Color.fromARGB(255, 238, 243, 247),
+          shadowDarkColorEmboss: const Color.fromARGB(255, 213, 224, 235),
+          boxShape: NeumorphicBoxShape.roundRect(BorderRadius.circular(100)),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Neumorphic(
+            style: NeumorphicStyle(
+              depth: 3,
+              color: Colors.white,
+              intensity: 5.0,
+              shadowLightColor: const Color.fromARGB(255, 240, 246, 250),
+              shadowDarkColor: const Color.fromARGB(255, 199, 212, 224),
+              boxShape: NeumorphicBoxShape.roundRect(
+                BorderRadius.circular(100),
+              ),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 12),
+              child: Row(
+                children: [
+                  const Icon(
+                    Icons.analytics,
+                    size: 24,
+                    color: Color(0xFF01579B),
+                  ),
+                  const SizedBox(width: 12),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(
+                        height: 20,
+                        child: isLoading
+                            ? const SizedBox(
+                                width: 16,
+                                height: 16,
+                                child: CircularProgressIndicator(
+                                  strokeWidth: 2,
+                                ),
+                              )
+                            : Text(
+                                value.toString(),
+                                style: const TextStyle(
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                      ),
+                      const SizedBox(height: 2),
+                      Text(
+                        title,
+                        style: const TextStyle(
+                          fontSize: 11,
+                          color: Colors.black54,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  BarChartData chartData(List<Inventory> list) {
+    List<Inventory> top10List = list.take(10).toList();
+
+    final maxQuantity = top10List.isNotEmpty
+        ? top10List.map((i) => i.quantityOnHand).reduce(max).toDouble()
+        : 0.0;
+    final maxY = (maxQuantity > 0) ? maxQuantity + 5 : 10.0;
+
+    return BarChartData(
+      alignment: BarChartAlignment.spaceAround,
+      maxY: maxY,
+      minY: 0,
+      barTouchData: BarTouchData(enabled: true),
+      titlesData: FlTitlesData(
+        bottomTitles: AxisTitles(
+          sideTitles: SideTitles(
+            showTitles: true,
+            getTitlesWidget: (val, meta) => _bottomTitles(val, meta, top10List),
+            interval: 1,
+          ),
+        ),
+        leftTitles: AxisTitles(sideTitles: SideTitles(showTitles: true)),
+        topTitles: AxisTitles(sideTitles: SideTitles(showTitles: false)),
+        rightTitles: AxisTitles(sideTitles: SideTitles(showTitles: false)),
+      ),
+      gridData: FlGridData(show: true),
+      barGroups: List.generate(top10List.length, (i) {
+        final inv = top10List[i];
+        final y = (inv.quantityOnHand ?? 0).clamp(0, maxY).toDouble();
+        return BarChartGroupData(
+          x: i,
+          barRods: [BarChartRodData(toY: y, color: Colors.lightBlue)],
+        );
+      }),
+    );
+  }
+
+  Widget buildChart(String title, List<Inventory> data, bool isLoading) {
+    return SizedBox(
+      height: 500, // fixed stack height
+      child: Card(
+        margin: const EdgeInsets.symmetric(vertical: 12),
+        elevation: 3,
+        child: Padding(
+          padding: const EdgeInsets.all(8),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(title, style: headerStyle),
+              const SizedBox(height: 8),
+              SizedBox(
+                width: 300,
+                height: 400, // or whatever height fits your UI
+                child: isLoading
+                    ? const Center(child: CircularProgressIndicator())
+                    : data.isEmpty
+                    ? const Center(child: Text('No data'))
+                    : BarChart(chartData(data)),
+              ),
+            ],
+          ),
         ),
       ),
-      child: Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: Neumorphic(
-          style: NeumorphicStyle(
-            depth: 3,
-            color: Colors.white,
-            intensity: 5.0,
-            shadowLightColor: const Color.fromARGB(255, 240, 246, 250),
-            shadowDarkColor: const Color.fromARGB(255, 199, 212, 224),
-            boxShape: NeumorphicBoxShape.roundRect(
-              BorderRadius.circular(100),
-            ),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 12),
-            child: Row(
-              children: [
-                const Icon(Icons.analytics, size: 24, color: Color(0xFF01579B)),
-                const SizedBox(width: 12),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    SizedBox(
-                      height: 20,
-                      child: isLoading
-                          ? const SizedBox(
-                              width: 16,
-                              height: 16,
-                              child: CircularProgressIndicator(strokeWidth: 2),
-                            )
-                          : Text(
-                              value.toString(),
-                              style: const TextStyle(
-                                  fontSize: 14, fontWeight: FontWeight.bold),
-                            ),
-                    ),
-                    const SizedBox(height: 2),
-                    Text(
-                      title,
-                      style: const TextStyle(fontSize: 11, color: Colors.black54),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
-    ),
-  );
-}
+    );
+  }
 
-
-
-
-
-  Widget buildLeftColumn() {
-    return Column(
-      children: [
-        SizedBox(
-          width: 300,
-          height: 250,
-          child: Card(
-            elevation: 4,
-            child: Column(
-              children: [
-                const Text("No. of users"),
-                ListTile(
-                  leading: const Icon(Icons.person),
-                  title: Text("sample total"),
-                  subtitle: const Text("Total Customers"),
-                ),
-                ListTile(
-                  leading: const Icon(Icons.factory),
-                  title: Text("sample total"),
-                  subtitle: const Text("Total Suppliers"),
-                ),
-              ],
-            ),
-          ),
-        ),
-        const SizedBox(height: 30),
-        SizedBox(
-          width: 725,
-          height: 250,
-          child: Card(
-            elevation: 4,
-            child: LineChart(
-              LineChartData(
-                lineBarsData: [
-                  LineChartBarData(
-                    spots: [
-                      FlSpot(0, 0),
-                      FlSpot(3, 2),
-                      FlSpot(6, 10)
-                    ]
-                  )
-                ]
-              )
-            ),
-          ),
-        )
-      ],
+  Widget _bottomTitles(double value, TitleMeta meta, List<Inventory> list) {
+    const style = TextStyle(color: Colors.black, fontSize: 10);
+    final idx = value.toInt();
+    final label = (idx >= 0 && idx < list.length) ? list[idx].brand : '';
+    return SideTitleWidget(
+      meta: meta,
+      space: 4,
+      child: Text(label, style: style),
     );
   }
 
@@ -264,7 +348,11 @@ Widget summaryCard(String title, int value, {bool isLoading = false}) {
               children: [
                 Text(
                   'Biotech App',
-                  style: TextStyle(color: Colors.white, fontSize: 24, fontWeight: FontWeight.bold),
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
                 SizedBox(height: 5),
                 Text(
