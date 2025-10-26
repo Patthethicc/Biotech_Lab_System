e2578c8571f4d97cb8389c55dbce662ec9ea856e
420Rain <dulatrerain@gmail.com>
2025-10-26 14:59:47 +0800

    chore: removed unnecessary imports

--- frontend/lib/models/ui/stock_alert.dart
+++ frontend/lib/models/ui/stock_alert.dart
@@ -1,4 +1,3 @@
-// import 'package:flutter/material.dart';
 import 'package:frontend/services/stock_alert_service.dart';
 import 'package:flutter_neumorphic_plus/flutter_neumorphic.dart';
 import 'package:frontend/models/api/inventory_payload.dart';
