b097a4d6faa6828a037d54ba3ab78ceae0909db9
420Rain <dulatrerain@gmail.com>
2025-10-30 17:41:44 +0800

    feat: expanded location model for CRUD operations

--- frontend/lib/models/api/location.dart
+++ frontend/lib/models/api/location.dart
@@ -1,6 +1,23 @@
 class Location {
-  final int id;
-  final String name;
+  int? locationId;
+  String locationName;
 
-  Location({required this.id, required this.name});
+  Location({
+    this.locationId, 
+    required this.locationName
+  });
+
+  factory Location.fromJson(Map<String, dynamic> json){
+    return Location(
+      locationId: json['locationId'],
+      locationName: json['locationName']
+    );
+  }
+
+  Map<String, dynamic> toJson(){
+    return {
+      "locationId": locationId,
+      "locationName": locationName
+    };
+  }
 }
